a52941b9beb01e58e116a0582f86d34c080239a9
Alice <alicenjoroge707@gmail.com>
2020-03-25 20:57:42 +0300

    intergrate a template

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles/fontawesome-free/css/all.css';
+import './styles/sb-admin-2.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
