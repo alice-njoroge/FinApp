9f15ebb0627280b33d44e8cbb116f2c203e4ceeb
Alice <alicenjoroge707@gmail.com>
2020-03-26 12:02:08 +0300

    create an income, ensure the recorder is currently logged user

--- Server/controllers/IncomesController.js
+++ Server/controllers/IncomesController.js
@@ -1,15 +1,51 @@
 const Joi = require('joi');
 const db = require('../models');
 
-const index = async (req, res)=>{
+const index = async (req, res) => {
     try {
-        const income = await db.incomes.findAll({include:"source"});
+        const income = await db.incomes.findAll({include: "source"});
+        return res.json(income);
+    } catch (e) {
+        throw e;
+    }
+};
+const create = async (req, res) => {
+    const month = req.body.month;
+    const year = req.body.year;
+    const amount = req.body.amount;
+    const income_source_id = req.body.income_source_id;
+    const description = req.body.description;
+
+    const schema = Joi.object().keys({
+        month: Joi.number().required(),
+        year: Joi.number().required(),
+        amount: Joi.number().integer().min(0).required(),
+        income_source_id: Joi.number().required(),
+        description: Joi.string().allow('').required()
+    });
+    const results = Joi.validate(req.body, schema);
+    const valid = results.error == null;
+    if (!valid) {
+        return res.status(400).json({error:results.error.details});
+    }
+
+    try{
+        const income = await db.incomes.create({
+            month:month,
+            year:year,
+            amount:amount,
+            income_source_id:income_source_id ,
+            recorded_by_id : req.user.id,
+            description:description
+        });
         return res.json(income);
     }catch (e) {
-      throw e;
+        throw e;
     }
+
 };
 
 module.exports = {
-    index
+    index,
+    create
 };
--- Server/routes/incomes.js
+++ Server/routes/incomes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const IncomesController = require('../controllers/IncomesController');
 
 router.get('/', IncomesController.index);
+router.post('/', IncomesController.create);
 
 module.exports = router;
