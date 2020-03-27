1a7272ddc3d2477658763c1a6cd2ad40a3619940
Alice <alicenjoroge707@gmail.com>
2020-03-27 07:12:29 +0300

    update an income

--- Server/controllers/IncomesController.js
+++ Server/controllers/IncomesController.js
@@ -26,40 +26,82 @@ const create = async (req, res) => {
     const results = Joi.validate(req.body, schema);
     const valid = results.error == null;
     if (!valid) {
-        return res.status(400).json({error:results.error.details});
+        return res.status(400).json({error: results.error.details});
     }
 
-    try{
+    try {
         const income = await db.incomes.create({
-            month:month,
-            year:year,
-            amount:amount,
-            income_source_id:income_source_id ,
-            recorded_by_id : req.user.id,
-            description:description
+            month: month,
+            year: year,
+            amount: amount,
+            income_source_id: income_source_id,
+            recorded_by_id: req.user.id,
+            description: description
         });
         return res.json(income);
-    }catch (e) {
+    } catch (e) {
         throw e;
     }
 
 };
-const show = async (req, res)=>{
+const show = async (req, res) => {
     const id = req.params.id;
-try {
-    const income = await db.incomes.findByPk(id);
-    if (!income){
-        return res.status(404).json({message:"Income not found"})
-    }
-    return res.json(income);
+    try {
+        const income = await db.incomes.findByPk(id);
+        if (!income) {
+            return res.status(404).json({message: "Income not found"})
+        }
+        return res.json(income);
 
-}catch (e) {
-    throw e;
-}
+    } catch (e) {
+        throw e;
+    }
+};
+const update = async (req, res) => {
+    const month = req.body.month;
+    const year = req.body.year;
+    const amount = req.body.amount;
+    const income_source_id = req.body.income_source_id;
+    const recorded_by_id = req.user.id;
+    const description = req.body.description;
+
+    const schema = Joi.object().keys({
+        month: Joi.number().required(),
+        year: Joi.number().required(),
+        amount: Joi.number().integer().min(0).required(),
+        income_source_id: Joi.number().required(),
+        description: Joi.string().allow('').required()
+    });
+    const results = Joi.validate(req.body, schema);
+    const valid = results.error == null;
+    if (!valid) {
+        return res.status(422).json({error: results.error.details});
+    }
+    try {
+        const income = await db.incomes.findByPk(req.params.id);
+        if (!income){
+            return res.status(404).json({message:"Income not found!"})
+        }
+        income.month = month;
+        income.year = year;
+        income.amount = amount;
+        income.income_source_id = income_source_id;
+        income.recorded_by_id = recorded_by_id;
+        income.description = description;
+        income.save();
+
+        return res.json(income);
+    } catch (e) {
+        throw e;
+
+    }
+
+
 };
 
 module.exports = {
     index,
     create,
-    show
+    show,
+    update
 };
--- Server/routes/incomes.js
+++ Server/routes/incomes.js
@@ -5,5 +5,6 @@ const IncomesController = require('../controllers/IncomesController');
 router.get('/', IncomesController.index);
 router.post('/', IncomesController.create);
 router.get('/:id', IncomesController.show);
+router.put('/:id', IncomesController.update);
 
 module.exports = router;
